236070b0dd1519e72a32a7846fec3f66a502a019
CaptainOachkatzl <dave.hofer@hotmail.com>
2021-12-08 14:40:26 +0100

    added delayed request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use rocket::*;
+use rocket::tokio::time::*;
 
 #[get("/")]
 async fn index() -> &'static str {
@@ -12,10 +13,16 @@ async fn greet(name: &str) -> String {
     return format!("hi, {}!", name);
 }
 
+#[get("/delay")]
+async fn delay() -> &'static str {
+    sleep(Duration::from_secs(3)).await;
+    return "well, this took a while...";
+}
+
 #[rocket::main]
 async fn main() {
     let server_status = rocket::build()
-        .mount("/", routes![index, greet])
+        .mount("/", routes![index, greet, delay])
         .launch()
         .await;
 
